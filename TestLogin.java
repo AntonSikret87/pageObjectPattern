669d47017a9b493ce0545f499aaafe77ef2e0d42
Anton <antonkarabeinikov@gmail.com>
2016-12-18 20:45:24 +0200

    11
    11

---
+++ TestLogin.java
@@ -0,0 +1,40 @@
+package pageObjectPattern;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ab83625
+ * Date: 10.11.2010
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLogin extends TestCase {
+   public void setUp() {
+        // Инициализация контекста.
+        Context.initInstance(Context.BROWSER_IE, "http://www.testtesttestlogin.com");
+    }
+
+   public void testLoginLogout() {
+        String userName = "tester";
+        String password = "testPass";
+        LoginPage loginPage = LoginPage.openLoginPage();
+        HomePage homePage = loginPage.login(userName, password);
+        assertEquals(userName, homePage.getLoggedinUserName());
+        homePage.logout();
+    }
+
+    public void testInvalidLogin() {
+        String userName = "$tester@#";
+        String password = "********";
+        String expectedMessage = "Invalid username or password";
+        LoginPage loginPage = LoginPage.openLoginPage();
+        ErrorLoginPage errorLoginPage = loginPage.loginInvalid(userName, password);
+        assertEquals(expectedMessage, errorLoginPage.getErrorMessage());
+        errorLoginPage.backToLoginPage();
+    }
+
+    protected void tearDown() throws Exception {
+        // закрытие браузера
+       Context.getInstance().close();
+    }
+}
